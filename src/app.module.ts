29dd282aaba084725cfa5e650af74be3a14d7a6a
Gifar Al Anshar <gifar.anshar12@gmail.com>
2023-01-27 10:32:42 +0700

    setup typeorm and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'simple_medium_web',
+      entities: [],
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: [],
 })
